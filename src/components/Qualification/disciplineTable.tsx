5bf011fbdd2e83018da186081031691a384178f9
PavelRunchev <Setty_ii@abv.bg>
2024-06-26 10:34:43 +0300

    fixed Table Tu-gab number symbol!

--- src/components/Qualification/disciplineTable.tsx
+++ src/components/Qualification/disciplineTable.tsx
@@ -6,23 +6,23 @@ import TableModel from './table-model';
 import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 
 export default function DisciplineTable() {
-
-  return (
-    <div className='table-container my-5'>
-        <MDBTable striped hover>
-            <MDBTableHead dark>
-                <tr className='table-grid'>
-                    <th scope='col'>\u2116</th>
-                    <th scope='col'>Discipline</th>
-                    <th scope='col'>Description</th>
-                    <th scope='col'>Grade</th>
-                </tr>
-            </MDBTableHead>
-            <MDBTableBody>
-                {tuGabData.map((el, i) => <TableModel key={i} data={{...{index: i + 1}, ...el}}/>)}
-            </MDBTableBody>
-        </MDBTable>
-    </div>
-  
-  );
+    const numberSymbol: String = '\u2116';
+    return (
+        <div className='table-container my-5'>
+            <MDBTable striped hover>
+                <MDBTableHead dark>
+                    <tr className='table-grid'>
+                        <th scope='col'>{numberSymbol}</th>
+                        <th scope='col'>Discipline</th>
+                        <th scope='col'>Description</th>
+                        <th scope='col'>Grade</th>
+                    </tr>
+                </MDBTableHead>
+                <MDBTableBody>
+                    {tuGabData.map((el, i) => <TableModel key={i} data={{...{index: i + 1}, ...el}}/>)}
+                </MDBTableBody>
+            </MDBTable>
+        </div>
+    
+    );
 }
